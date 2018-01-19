e144e38e9376252c8c98e77051bd65f9f40d770e
KonstantinKrylov <kr-konstantin@yandex.ru>
2018-01-19 19:00:59 +0300

    Strings

---
+++ src/strings/JavaSubstring.java
@@ -0,0 +1,17 @@
+package strings;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+    /**
+     * https://www.hackerrank.com/challenges/java-substring/problem
+     */
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start, end));
+    }
+}
